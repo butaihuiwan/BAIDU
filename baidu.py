9d1b3840b4de05e5999b15c59850b01c73ec2941
butaihuiwan <925747399@qq.com>
2019-11-20 10:44:34 +0800

    百度-删除Chrome() 中 指定的chomedriver 的路径

--- baidu.py
+++ baidu.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 opt = webdriver.ChromeOptions()
 opt.headless = True
 
-driver = webdriver.Chrome('C:\\ProgramFiles(x86)\\Google\\Chrome\\Application\\chromedriver.exe', options=opt)
+driver = webdriver.Chrome(options=opt)
 driver.get('http://www.baidu.com')
 print('百度 ok')
 driver.quit()
