0daa6e7a5adc310417388eba4b8f10445c4b1082
butaihuiwan <925747399@qq.com>
2021-08-23 20:01:02 +0800

    ceshi

--- baidu.py
+++ baidu.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 opt = webdriver.ChromeOptions()
 opt.headless = True
 
-driver = webdriver.Chrome(options=opt)
+driver = webdriver.Chrome(executable_path='D:\guge\chromedriver_win32\chromedriver.exe', options=opt)
 driver.get('http://www.baidu.com')
 print('百度 ok')
 driver.quit()
